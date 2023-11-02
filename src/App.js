284329a27554d28d17c497d513bb9d72dedba313
maurisq12 <maurisq@hotmail.com>
2023-11-01 19:23:48 -0600

    arranque

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import logo from './Media/logo.png';
 import './App.css';
+import prueba from './ServiciosIA/chatPDF';
+
+
 
 function App() {
   return (
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {prueba()}
         </a>
       </header>
     </div>
---
+++ src/ServiciosIA/chatPDF.js
@@ -0,0 +1,5 @@
+
+
+export default function prueba(){
+    return 'hola esta es la respuesta del API';
+}
